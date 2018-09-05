7a27f2b2766ab28fddd9cbdc22f8080f2c1925df
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-05 17:19:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,9 @@
 class Transfer
   attr_reader :name
   attr_accessor :money
-  def initialize(name, money)
+  def initialize(name, name2, money)
     @name = name
+    @name2 = name2
     @money = money
   end
 
